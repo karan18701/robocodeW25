bdc17235c9dc30494d65b329a9e379f0c7692ead
karan18701 <107426756+karan18701@users.noreply.github.com>
2025-04-07 12:00:34 -0400

    Update JarExtractor.java

--- robocode.repository/src/main/java/net/sf/robocode/repository/packager/JarExtractor.java
+++ robocode.repository/src/main/java/net/sf/robocode/repository/packager/JarExtractor.java
@@ -65,22 +65,31 @@ public class JarExtractor {
 	}
 
 	public static void extractFile(File dest, JarInputStream jarIS, JarEntry entry) throws IOException {
-		File out = new File(dest, entry.getName());
-		File parentDirectory = new File(out.getParent());
+		File canonicalDestDir = destDir.getCanonicalFile();
 
+		File outFile = new File(destDir, entry.getName());
+		File canonicalOutFile = outFile.getCanonicalFile();
+
+		if (!canonicalOutFile.getPath().startsWith(canonicalDestDir.getPath() + File.separator)) {
+			Logger.logError("Entry is outside the target dir: " + entry.getName());
+			return;
+		}
+
+		File parentDirectory = new File(canonicalOutFile.getParent());
 		if (!parentDirectory.exists() && !parentDirectory.mkdirs()) {
 			Logger.logError("Cannot create dir: " + parentDirectory);
+			return;
 		}
+
 		FileOutputStream fos = null;
 		BufferedOutputStream bos = null;
 		byte[] buf = new byte[2048];
 
 		try {
-			fos = new FileOutputStream(out);
+			fos = new FileOutputStream(canonicalOutFile);
 			bos = new BufferedOutputStream(fos);
 
 			int num;
-
 			while ((num = jarIS.read(buf, 0, 2048)) != -1) {
 				bos.write(buf, 0, num);
 			}
